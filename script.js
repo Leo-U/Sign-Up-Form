26083485cc1f985d12ccf04d5d0be3a1cd76c013
Joel Howard <joelhoward2@gmail.com>
2023-07-25 12:38:01 -0400

    Modify script.js to validate form based on input instead of just clicking submit

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-document.getElementById('regForm').addEventListener('submit', function (event) {
-  let password = document.getElementById('pword').value;
-  let confirmPassword = document.getElementById('pword-conf').value;
-  console.log(password);
-  console.log(confirmPassword);
-  if (password !== confirmPassword) {
-    event.preventDefault();
-    document.getElementById('missmatch-msg').textContent = 'Passwords do not match';
-    let elements = document.getElementsByClassName("pwords");
+let password = document.getElementById('pword');
+let confirmPassword = document.getElementById('pword-conf');
+let message = document.getElementById('missmatch-msg');
+let registerForm = document.getElementById('regForm');
+
+function validatePassword() {
+  let elements = document.getElementsByClassName("pwords");
+
+  if (password.value != confirmPassword.value) {
+    message.textContent = 'Passwords do not match';
     for (let i = 0; i < elements.length; i++) {
       elements[i].style.borderColor = "red";
     }
   } else {
-    document.getElementById('missmatch-msg').textContent = '';
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].style.borderColor = "#04b34c";
+    }
+    message.textContent = '';
   }
-});
+}
+
+password.oninput = validatePassword;
+confirmPassword.oninput = validatePassword;
+
+registerForm.onsubmit = function (event) {
+  if (password.value != confirmPassword.value) {
+    event.preventDefault();
+  }
+}
